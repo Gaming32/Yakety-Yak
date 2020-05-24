8053fe5a3fb8cf409843b5547cde4594635937c8
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-05-24 17:21:06 -0500

    audio works

---
+++ YaketyYak/Assets/Scripts/NetworkUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkUser : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(Microphone.devices[0], true, 2, 44100);
+        audioSource.Play();
+    }
+}
